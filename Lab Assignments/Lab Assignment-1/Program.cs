95b8eb2f00f1b4c80abd078f41a68a675fc5eef7
Madan Tirwa <madan.acad.edu@gmail.com>
2020-02-12 11:43:59 -0500

    Updated on 2/12/20 @ 11:40

--- Lab Assignments/Lab Assignment-1/Program.cs
+++ Lab Assignments/Lab Assignment-1/Program.cs
@@ -6,50 +6,57 @@ namespace Lab_Assignment_1
     {
         static void Main(string[] args)
         {
-            ////////////////////////////////////////////////////////////////////////////////////////////////
-            string firstName;
-            string middleInitial;
-            string lastName;
-            string fullName;
+            #region Part 1 - String Concatenation
 
             Console.Write("What is your first name? --> ");
-            firstName = Console.ReadLine();
-            Console.Write("What is your middle initial? --> ");
-            middleInitial = Console.ReadLine();
-            Console.Write("What is your last name? --> ");
-            lastName = Console.ReadLine();
-            fullName = (firstName + " " + middleInitial + " " + lastName);
+            string firstName = Console.ReadLine();
 
-            /////////////////////////////////////////////////////////////////////////////////////////////
-            int age;
-            bool isCitizen;
-            bool canVote;
+            Console.Write("What is your middle initial? --> ");
+            string middleInitial = Console.ReadLine();
+
+            Console.Write("What is your last name? --> ");
+            string lastName = Console.ReadLine();
+
+            string fullName = (firstName + " " + middleInitial + " " + lastName);
+
+            #endregion
+            //////////////////////////////////////////////////////////////
+            #region Part 2 - Arithmetic Expression
 
             Console.Write("What is your age? --> ");
-            age = int.Parse(Console.ReadLine());
-            Console.Write("Are you a US citizen? --> ");
-            isCitizen = Console.ReadLine().ToUpper().StartsWith("Y");
-            Console.Write("Can you vote? --> ");
-            canVote = Console.ReadLine().ToUpper().StartsWith("Y");
-
-            /////////////////////////////////////////////////////////////////////////////////////////////
-            int heightFeet;
-            double heightInches;
-            double totalHeightCM;
+            int age = int.Parse(Console.ReadLine());
 
             Console.Write("What is your height? (only feet part) --> ");
-            heightFeet = int.Parse(Console.ReadLine());
-            Console.Write("What is your height? (only inches part) --> ");
-            heightInches = double.Parse(Console.ReadLine());
-            totalHeightCM = (((12 * heightFeet) + (heightInches)) * 2.54);
+            int heightFeetOnly = int.Parse(Console.ReadLine());
 
-            /////////////////////////////////////////////////////////////////////////////////////////////
+            Console.Write("What is your height? (only inches part) --> ");
+            double heightInchesOnly = double.Parse(Console.ReadLine());
+
+            const int inchesPerFt = 12;
+            const double cmPerInch = 2.54;
+
+            double heightFtToInches = (heightFeetOnly * inchesPerFt) + heightInchesOnly;
+            double totalHeightCM = (heightFtToInches * cmPerInch);
+
+            #endregion
+            //////////////////////////////////////////////////////////////
+            #region Part 3 - Boolean Expression
+
+            Console.Write("Are you a US citizen? --> ");
+            bool isCitizen = Console.ReadLine().ToUpper().StartsWith("Y");
+
+            bool canVote = age >= 18 && isCitizen;
+
+            #endregion
+            /////////////////////////////////////////////////////////////
+            #region Print
+
             Console.WriteLine("Hello " + fullName);
-            Console.WriteLine("Your height is " + totalHeightCM + "cm, and ");
-            Console.WriteLine("Your voting status is " + canVote);
+            Console.WriteLine("Your height in cm is " + totalHeightCM + "cm, and ");
+            Console.WriteLine("Your voting eligibility is " + canVote);
             Console.WriteLine("Press any key to end this program!");
 
-            /////////////////////////////////////////////////////////////////////////////////////////////
+            #endregion
         }
     }
 }
