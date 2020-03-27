4e5214e4324cfb0d2d93928dcdb4e8e317c4c7a5
Madan Tirwa <madan.acad.edu@gmail.com>
2020-03-26 21:55:41 -0400

    Added Lab Assignment-2 on 3/26/2020

---
+++ Lab Assignments/Lab Assignment-2/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_Assignment_2
+{
+    class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public int Age;
+        public Person Spouse;
+        public double SumofAllAges;
+        public static int Count;
+        public static double SumOfAllAges;
+
+
+        public string GetFullName()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+
+        public void PrintNameAndAge()
+        {
+            Console.WriteLine(GetFullName() + ", " + this.Age);
+        }
+
+        public void GetAnswers()
+        {
+            Console.Write("Enter Your First Name: ");
+            this.FirstName = Console.ReadLine();
+
+            Console.Write("Enter Your Last Name: ");
+            this.LastName = Console.ReadLine();
+
+            Console.Write("Enter Your Age: ");
+            this.Age = int.Parse(Console.ReadLine());
+
+            this.Spouse = new Person();
+
+            Console.Write("Enter Your Spouse First Name: ");
+            this.Spouse.FirstName = Console.ReadLine();
+
+            this.Spouse.LastName = this.LastName;
+
+            Console.Write("Enter Your Spouse Age: ");
+            this.Spouse.Age = int.Parse(Console.ReadLine());
+
+            Person.Count += 2;
+            Person.SumOfAllAges += this.Age + this.Spouse.Age;
+        }
+
+        public static double AvgAge()
+        {
+            return (Person.SumOfAllAges / Person.Count);
+        }
+
+    }
+}
+
---
+++ Lab Assignments/Lab Assignment-2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lab_Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            Person p2 = new Person();
+
+            Console.WriteLine("[Person 1 Info]");
+            p1.GetAnswers();
+            Console.WriteLine();
+
+            Console.WriteLine("[Person 2 Info]");
+            p2.GetAnswers();
+            Console.WriteLine();
+
+            Console.WriteLine("[Printing Results]");
+            p1.PrintNameAndAge();
+            p2.PrintNameAndAge();
+            p1.Spouse.PrintNameAndAge();
+            p2.Spouse.PrintNameAndAge();
+            Console.WriteLine();
+
+            Console.WriteLine("Average Age: " + Person.AvgAge());
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to end! ");
+            Console.ReadKey();
+        }
+    }
+}
