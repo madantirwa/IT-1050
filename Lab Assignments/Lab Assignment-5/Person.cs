8ada9b00326629f2a4ea295204133b42b568e379
Madan Tirwa <madan.acad.edu@gmail.com>
2020-04-28 08:15:25 -0400

    Added Lab Assignment #5

---
+++ Lab Assignments/Lab Assignment-5/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_Assignment_5
+{
+    class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public Person(string theirFirstName, string theirLastName)
+        {
+            this.FirstName = theirFirstName;
+            this.LastName = theirLastName;
+        }
+
+        public string Print()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+    }
+}
---
+++ Lab Assignments/Lab Assignment-5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab_Assignment_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] individuals = new Person[3];
+
+            individuals[0] = new Person("Madan", "Tirwa");
+            individuals[1] = new Person("Sarita", "Tirwa");
+            individuals[2] = new Person("Sawan", "Tirwa");
+
+            foreach (Person individual in individuals)
+            {
+                System.Console.WriteLine(individual.Print());
+            }
+        }
+    }
+}
